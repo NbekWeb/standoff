2b29858f52b3b722a84815d62fdd5189d502471c
NbekWeb <nbekdev@gmail.com>
2025-01-10 09:41:34 +0500

    open

---
+++ app/components/openCase/dots.vue
@@ -0,0 +1,53 @@
+<template>
+  <div class="flex gap-5 items-center h-6 justify-center">
+    <div
+      v-for="(span, index) in 3"
+      :key="index"
+      class="w-5 h-5 flex items-center"
+    >
+      <span
+        class="bg-orange-500 rounded-full animate-span"
+        :style="{ animationDelay: `${index * 0.4}s` }"
+      ></span>
+    </div>
+  </div>
+</template>
+
+<style scoped>
+@keyframes sizeChange {
+  0% {
+    width: 6px;
+    height: 6px;
+  }
+  20% {
+    width: 10px;
+    height: 10px;
+  }
+  40% {
+    width: 16px;
+    height: 16px;
+  }
+  60% {
+    width: 10px;
+    height: 10px;
+  }
+  80% {
+    width: 6px;
+    height: 6px;
+  }
+  100% {
+    width: 6px;
+    height: 6px;
+  }
+}
+
+.animate-span {
+  animation: sizeChange 1.2s infinite ease-in-out; /* Duration of 6s to fit all 3 spans */
+}
+
+span {
+  display: inline-block;
+  animation-duration: 1.2s; /* Same duration for all spans */
+  animation-timing-function: ease-in-out;
+}
+</style>
--- app/components/openCase/slide.vue
+++ app/components/openCase/slide.vue
@@ -1,9 +1,8 @@
 <script setup>
 import { Swiper, SwiperSlide } from "swiper/vue";
-import { Autoplay, Pagination } from "swiper/modules";
+import { Autoplay } from "swiper/modules";
 
 import "swiper/css";
-import "swiper/css/pagination";
 
 const props = defineProps({
   type: {
@@ -11,124 +10,150 @@ const props = defineProps({
     default: 1,
   },
 });
+
+const opened = ref(false);
+
+onMounted(() => {
+  setTimeout(() => {
+    opened.value = true;
+  }, 3000);
+});
+
+const toggleOpened = () => {
+  opened.value = false;
+  setTimeout(() => {
+    opened.value = true;
+  }, 3000);
+};
 </script>
 
 <template>
-  <client-only>
-    <div>
-      <div
-        v-if="type == 1"
-        class="p-5 relative mb-12 rounded-xs bg-dark-160 horizontal"
-      >
-        <div class="">
-          <Swiper
-            :slidesPerView="4.8"
-            :loop="true"
-            :space-between="20"
-            :modules="[Autoplay, Pagination]"
-            :autoplay="{ delay: 2500 }"
-            :pagination="{
-              clickable: true,
-              dynamicBullets: true,
-              dynamicMainBullets: 1,
-            }"
-            class=""
-          >
-            <SwiperSlide v-for="i in 20" :key="i">
-              <div
-                class="rounded-xs relative"
+  <div>
+    <div
+      v-if="type == 1"
+      class="p-5 relative rounded-xs bg-dark-160 horizontal"
+    >
+      <div class="">
+        <div class="grid-5 gap-5">
+          <div v-for="i in 5" :key="i" class="relative sa1">
+            <div
+              class="rounded-xs relative"
+              :class="
+                i % 4 == 1
+                  ? 'bg-orange'
+                  : i % 4 == 2
+                  ? 'bg-blue '
+                  : i % 4 == 3
+                  ? 'bg-pink relative z-2'
+                  : 'bg-blue2'
+              "
+            >
+              <NuxtImg src="/images/awm.png" class="w-full py-14 px-1" />
+              <span
+                class="absolute bottom-1 rounded-xs border-b h-1 centerX"
                 :class="
                   i % 4 == 1
-                    ? 'bg-orange'
+                    ? 'bg-orange-500'
                     : i % 4 == 2
-                    ? 'bg-blue'
+                    ? 'bg-blue-500'
                     : i % 4 == 3
-                    ? 'bg-pink'
-                    : 'bg-blue2'
+                    ? 'bg-pink-500'
+                    : 'bg-blue-700'
                 "
               >
-                <NuxtImg src="/images/awm.png" class="w-full py-14 px-1" />
-                <span
-                  class="absolute bottom-1 rounded-xs border-b h-1 centerX"
-                  :class="
-                    i % 4 == 1
-                      ? 'bg-orange-500'
-                      : i % 4 == 2
-                      ? 'bg-blue-500'
-                      : i % 4 == 3
-                      ? 'bg-pink-500'
-                      : 'bg-blue-700'
-                  "
-                >
-                </span>
-              </div>
-            </SwiperSlide>
-          </Swiper>
-          <NuxtImg
-            src="/images/nav.png"
-            class="rotate-90 w-5 absolute centerX -top-1"
-          />
-          <NuxtImg
-            src="/images/nav.png"
-            class="-rotate-90 w-5 absolute centerX -bottom-1"
-          />
-        </div>
-      </div>
-      <div v-else class="flex flex-col gap-5">
-        <div
-          v-for="i in type == 10 ? 2 : 1"
-          :key="i"
-          class="gap-5 vertical"
-          :class="
-            type == 2
-              ? 'grid-2'
-              : type == 3
-              ? 'grid-3'
-              : type == 4
-              ? 'grid-4'
-              : 'grid-5'
-          "
-        >
-          <div
-            class="rounded-xs bg-dark-160"
-            v-for="i in type > 5 ? 5 : type"
-            :key="i"
-          >
-            <Swiper
-              :slidesPerView="2.7"
-              :loop="true"
-              :space-between="20"
-              :modules="[Autoplay, Pagination]"
-              :autoplay="{ delay: 2500 }"
-              :pagination="{
-                clickable: true,
-                dynamicBullets: true,
-                dynamicMainBullets: 1,
-              }"
-              direction="vertical"
-              class=""
-              style="height: 260px"
-            >
-              <SwiperSlide v-for="i in 20" :key="i">
-                <div class="flex items-center justify-center">
-                  <NuxtImg
-                    src="/images/awm.png"
-                    :class="type > 4 ? 'h-20' : 'h-25'"
-                  />
-                </div>
-              </SwiperSlide>
-            </Swiper>
+              </span>
+            </div>
           </div>
         </div>
+        <NuxtImg
+          src="/images/nav.png"
+          class="rotate-90 z-2 w-5 absolute centerX -top-1"
+        />
+        <NuxtImg
+          src="/images/nav.png"
+          class="-rotate-90 w-5 z-2 absolute centerX -bottom-1"
+        />
       </div>
       <div
-        class="flex justify-center text-sm text-grey-900 font-semibold"
-        :class="type > 4 ? 'mt-3' : 'mt-1'"
+        class="absolute top-0 left-0 opened w-full h-full rounded-xs"
+        v-show="opened"
+      ></div>
+    </div>
+    <div v-else class="flex flex-col gap-5">
+      <div
+        v-for="i in type == 10 ? 2 : 1"
+        :key="i"
+        class="gap-5 vertical relative"
+        :class="
+          type == 2
+            ? 'grid-2 '
+            : type == 3
+            ? 'grid-3 '
+            : type == 4
+            ? 'grid-4'
+            : 'grid-5'
+        "
       >
+        <div
+          class="rounded-xs bg-dark-160"
+          v-for="i in type > 5 ? 5 : type"
+          :key="i"
+          :class="
+            i % 4 == 1
+              ? 'bg-orange'
+              : i % 4 == 2
+              ? 'bg-blue '
+              : i % 4 == 3
+              ? 'bg-pink relative z-2'
+              : 'bg-blue2'
+          "
+        >
+          <div class="flex flex-col gap-5">
+            <div
+              v-for="i in 3"
+              :key="i"
+              class="flex items-center justify-center"
+            >
+              <NuxtImg
+                src="/images/awm.png"
+                :class="type > 4 ? 'h-20' : 'h-25'"
+              />
+            </div>
+          </div>
+        </div>
+        <span class="absolute centerY -left-1">
+          <NuxtImg src="/images/nav.png" class="h-5" />
+        </span>
+        <span class="absolute centerY -right-1 rotate-180">
+          <NuxtImg src="/images/nav.png" class="h-5" />
+        </span>
+      </div>
+    </div>
+    <div class="mt-5 flex flex-col gap-2" v-show="!opened">
+      <OpenCaseDots />
+      <div class="flex justify-center text-base text-grey-900 font-semibold">
         ОТКРЫВАЕТСЯ
       </div>
     </div>
-  </client-only>
+    <div class="mt-5 flex justify-between gap-20 px-20" v-show="opened">
+      <button
+        class="btn bg-dark-200 item text-base text-grey-900 font-semibold flex-grow h-14 flex items-center justify-center"
+      >
+        ПРОДАТЬ ЗА 50 G
+      </button>
+      <button
+        @click="toggleOpened"
+        class="btn btn-orange px-20 text-dark-200 text-base text-grey-900 font-semibold h-14 flex items-center justify-center"
+      >
+        ОТКРЫТЬ ЕЩЕ РАЗ
+      </button>
+      <button
+        class="btn bg-dark-200 item text-base text-grey-900 font-semibold flex-grow h-14 flex items-center justify-center"
+      >
+        ДОБАВИТЬ В КОНТРАКТ
+      </button>
+    </div>
+  </div>
 </template>
 
 <style scoped lang="scss">
@@ -162,5 +187,23 @@ const props = defineProps({
 }
 .-bottom-1 {
   bottom: -4px;
+}
+.-left-1 {
+  left: -4px;
+}
+
+.-right-1 {
+  right: -4px;
+}
+
+.opened {
+  background: rgba($dark-160, 0.9);
+}
+
+.item {
+  transition: 0.3s ease;
+  &:hover {
+    color: #fff;
+  }
 }
 </style>

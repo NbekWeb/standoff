c819d5ac45ae4e616b0685f85c2b569017475f71
NbekWeb <nbekdev@gmail.com>
2025-01-10 19:46:59 +0500

    faq

--- app/pages/referral.vue
+++ app/pages/referral.vue
@@ -14,14 +14,18 @@ function copyText(val) {
 }
 </script>
 <template>
-  <div class="container flex flex-col gap-5 text-grey-900 text-base">
+  <div
+    class="container overflow-x-hidden flex flex-col gap-5 text-grey-900 text-base"
+  >
     <h2 class="text-xl font-semibold text-center text-white">
       РЕФЕРАЛЬНАЯ СИСТЕМА
     </h2>
     <div class="grid-4 gap-5 font-semibold">
-      <div class="p-5 bg-dark-200 rounded-xs flex gap-5 items-center">
+      <div
+        class="p-5 col-span-2 bg-dark-200 rounded-xs flex gap-5 items-center"
+      >
         <NuxtImg src="/images/emblema.png" class="emblema rounded-xs" />
-        <div class="flex flex-col h-full justify-between w190">
+        <div class="flex flex-grow flex-col h-full justify-between w190 gap-5">
           <button class="h-14 btn w-full text-white bg-dark-300 rounded-xs">
             ONELEDAY
           </button>
@@ -30,12 +34,14 @@ function copyText(val) {
           </button>
         </div>
       </div>
-      <div class="bg-dark-200 rounded-xs flex gap-5 items-center">
+      <div class="bg-dark-200 col-span-2 rounded-xs flex gap-5 items-center">
         <NuxtImg src="/images/level1.png" class="w190 rounded-xs" />
-        <div class="p-5 flex flex-col h-full justify-between items-center">
+        <div
+          class="p-3 flex-grow flex flex-col h-full justify-between items-center"
+        >
           <div class="flex text-white flex-col items-center">
-            <span>ВАШ УРОВЕНЬ</span>
-            <span>1</span>
+            <span class="min-w-full flex-grow">ВАШ УРОВЕНЬ</span>
+            <span class="mt-2">1</span>
           </div>
           <span>0 ИЗ 1000</span>
         </div>
@@ -58,7 +64,7 @@ function copyText(val) {
     <div class="grid-2 gap-5">
       <div class="rounded-xs bg-dark-200 p-5">
         <span class="text-white"> ВАШ ИНВАЙТ-КОД: </span>
-        <div class="flex gap-5 h-14 mt-5">
+        <div class="flex gap-5 h-14 mt-5 sm">
           <div
             class="py-4 px-5 h-full rounded-xs bg-dark-300 flex gap-5 items-center flex-grow justify-between"
           >
@@ -76,7 +82,7 @@ function copyText(val) {
           </div>
           <button
             @click="toggle"
-            class="btn btn-orange w180 flex justify-center text-base font-semibold upercase h-full"
+            class="btn w-full btn-orange w180 flex justify-center text-base font-semibold upercase h-full"
           >
             {{ isReadonly ? "ИЗМЕНИТЬ" : "Сохранять" }}
           </button>
@@ -85,7 +91,7 @@ function copyText(val) {
 
       <div class="rounded-xs bg-dark-200 p-5">
         <span class="text-white">У ВАС ЕСТЬ ИНВАЙТ-КОД? </span>
-        <div class="flex gap-5 h-14 mt-5">
+        <div class="flex gap-5 h-14 mt-5 overflow-x-hidden sm">
           <div
             class="py-4 px-5 h-full rounded-xs bg-dark-300 flex gap-5 items-center flex-grow justify-between"
           >
@@ -98,8 +104,10 @@ function copyText(val) {
           </div>
           <button
             @click="() => (code = '')"
-            class="btn h-full  w180 flex justify-center text-base font-semibold upercase h-full"
-            :class="code ? 'btn-orange ' : 'point-none bg-dark-300 text-grey-900'"
+            class="btn h-full w180 flex justify-center text-base font-semibold upercase h-full"
+            :class="
+              code ? 'btn-orange ' : 'point-none bg-dark-300 text-grey-900'
+            "
           >
             АКТИВИРОВАТЬ
           </button>
@@ -128,7 +136,7 @@ function copyText(val) {
       <div
         v-for="i in 6"
         :key="i"
-        class="rounded-xs justify-between py-5 bg-dark-160 flex flex-col items-center"
+        class="rounded-xs text-sm justify-between py-5 bg-dark-160 flex flex-col items-center"
       >
         <div class="flex flex-col gap10 items-center flex-grow">
           <span class="text-white"> 1 УРОВЕНЬ </span>
@@ -171,12 +179,73 @@ function copyText(val) {
   height: 132px;
 }
 
-
-
 .item {
   transition: 0.3s ease;
   &:hover {
     color: #fff;
+  }
+}
+
+@media screen and (max-width: 1176px) {
+  .grid-6 {
+    grid-template-columns: repeat(3, minmax(0, 1fr));
+  }
+}
+
+@media screen and (max-width: 1024px) {
+  .grid-2 {
+    grid-template-columns: repeat(1, minmax(0, 1fr));
+  }
+}
+
+@media screen and (max-width: 768px) {
+  .p-5 {
+    padding: 10px;
+  }
+  .my-5 {
+    margin-top: 10px;
+    margin-bottom: 10px;
+  }
+  .mt-5 {
+    margin-top: 10px;
+  }
+  .gap-5 {
+    gap: 10px;
+  }
+  .py-5 {
+    padding-top: 10px;
+    padding-bottom: 10px;
+  }
+  .px-5 {
+    padding-left: 10px;
+    padding-right: 10px;
+  }
+  .col-span-2 {
+    grid-column: span 2 / span 2;
+  }
+  .w180 {
+    width: 100%;
+  }
+}
+
+@media screen and (max-width: 1280px) {
+  .grid-4 {
+    grid-template-columns: repeat(2, minmax(0, 1fr));
+  }
+}
+
+@media screen and (max-width: 640px) {
+  .sm {
+    flex-direction: column;
+  }
+  .sm.h-14 {
+    height: auto;
+  }
+  .grid-6 {
+    grid-template-columns: repeat(2, minmax(0, 1fr));
+    .text-sm {
+      font-size: 12px;
+    }
   }
 }
 </style>
